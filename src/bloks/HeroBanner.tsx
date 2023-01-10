2df83d9fc50674d3bd41ec9a0b6b27676711e5e2
Gerwald Gindra-Vady <gerwald.gindra-vady@edu.fh-joanneum.at>
2023-01-10 19:53:44 +0100

    heroBanner object-fit cover added

--- src/bloks/HeroBanner.tsx
+++ src/bloks/HeroBanner.tsx
@@ -5,9 +5,9 @@ import backgroundImage from "./../../public/background.jpg"
 const HeroBanner: FunctionComponent<any> = ({}) => {
     return (
         <div className={"relative m-auto flex max-w-full items-center justify-center p-4 min-h-[480px]"}>
-            <Image width={"1280"} height={"1280"} src={backgroundImage} alt={'background image'} className={"w-full h-full absolute"} loading={"eager"}/>
+            <Image width={"1280"} height={"1280"} src={backgroundImage} alt={'background image'} className={"h-full w-full object-cover absolute"} loading={"eager"}/>
             <div className={"relative text-center text-white"}>
-
+                <p>HeroBanner</p>
             </div>
         </div>
     )
